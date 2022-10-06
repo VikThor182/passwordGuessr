5c347c0bcccf4c2f9043f3a04dd40419ad7a560d
VikThor182 <victor.cousseau49@gmail.com>
2022-10-06 16:34:48 +0200

    Create Password model :construction:

---
+++ Password.py
@@ -0,0 +1,12 @@
+import Numbers
+import Words
+import Dates
+
+
+class Password:
+    def __init__(self, words, dates, numbers):
+        self.words = Words
+        self.dates = Dates
+        self.numbers = Numbers
+
+
